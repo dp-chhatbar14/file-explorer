3aebfb6c77808799a803355eadf6bc652c056de5
dp-chhatbar14 <deepchhatbar97@gmail.com>
2024-01-12 16:40:03 +0530

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -34,9 +34,21 @@ export default function App() {
     setExplorerData(updatedExplorer);
   };
 
+  const deleteNode = (nodeId) => {
+    const deleteNodeFromData = (explorer, nodeId) => {
+      let latestNode = explorer.items.map((obj) => {
+        if(obj.items.length === 0) {return obj}
+        return deleteNodeFromData(obj, nodeId)
+      }).filter((obj)=> obj.id===nodeId?false:true)
+      return {...explorer,items:latestNode}
+    };
+    let updatedExplorer = deleteNodeFromData(explorerData,nodeId);
+    setExplorerData(updatedExplorer);
+  };
+
   return (
     <div className="App">
-      <Folder insertNode={insertNode} explorer={explorerData}></Folder>
+      <Folder insertNode={insertNode} deleteNode={deleteNode} explorer={explorerData}></Folder>
     </div>
   );
 }
--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-function Folder({ insertNode, explorer }) {
+function Folder({ insertNode, deleteNode, explorer }) {
   const [expand, setExpand] = useState(false);
   const [inputExpand, setInputExpand] = useState({
     visible: false,
     isFolder: null,
   });
-
+  
   const handleCreateFolder = (e, isFolder) => {
     e.stopPropagation();
     setExpand(true);
@@ -33,7 +33,7 @@ function Folder({ insertNode, explorer }) {
           <div className="buttonContainer">
             <button onClick={(e) => handleCreateFolder(e, true)}>📁 +</button>
             <button onClick={(e) => handleCreateFolder(e, false)}>📄 +</button>
-            <button>❌</button>
+            {explorer.id==="1"?null:<button onClick={() => deleteNode(explorer.id)}>❌</button>}
           </div>
         </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
@@ -52,13 +52,18 @@ function Folder({ insertNode, explorer }) {
             </div>
           )}
           {explorer.items.map((ob) => {
-            return <Folder insertNode={insertNode} explorer={ob} key={ob.id} />;
+            return <Folder insertNode={insertNode} deleteNode={deleteNode} explorer={ob} key={ob.id} />;
           })}
         </div>
       </div>
     );
   } else {
-    return <span className="file">📄 {explorer.name}</span>;
+    return <div className="file">
+              <span>📄 {explorer.name}</span>
+              <div className="buttonContainer">
+                <button onClick={() => deleteNode(explorer.id)} >❌</button>
+              </div>
+            </div>
   }
 }
 export default Folder;
